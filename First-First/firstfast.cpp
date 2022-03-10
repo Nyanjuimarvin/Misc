f1c40cb9958f93dadc2f0964030a50d8e1449441
Nyanjuimarvin <marnyanjui@gmail.com>
2022-03-10 18:40:17 +0300

    Close

--- First-First/firstfast.cpp
+++ First-First/firstfast.cpp
@@ -2,7 +2,7 @@
 
 #include <iostream>;
 
-float firstFast(float x);
+float firstFast( float x );
 
 int main()
 {
@@ -13,22 +13,22 @@ int main()
 
 // Calculate f(x) = 1/sqrt x;
 
-float firstFast(float myFloat)
+float firstFast( float myFloat )
 {
 
     float halfFloat = 0.5 * myFloat;
 
     // Cast address of myFloat to  intPtr and dereference *Read Float as Int*
-    int temp = *(int *)&myFloat; // Bits
+    int temp = *(int *) &myFloat; // Bits
 
 
     // Start with simple notation :: find invsqrt if y = 1/sqrt y
     // log(1/sqrt y) = log (pow(y,-0.5)) = -0.5 log(y)
-    temp = 0x5f3759df - (temp >> 1); // ??
+    temp = 0x5f3759df - ( temp >> 1 ); // ??
 
     // cast address of temp to floatPtr and dereference
     //temp is now an approximation of the invsqrt of myFloat
-    myFloat = *(float *)&temp; // Bits
+    myFloat = *(float *) &temp; // Bits
 
     //Math method for almost accurate approximations
     myFloat = myFloat * ( 1.5f - halfFloat * myFloat *myFloat );
