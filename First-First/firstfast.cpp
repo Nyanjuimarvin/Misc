a303aa61c770d1694d9da939a5de6dce4f45be46
Nyanjuimarvin <marnyanjui@gmail.com>
2022-03-10 18:25:53 +0300

    Read results from confusing math step as a float

--- First-First/firstfast.cpp
+++ First-First/firstfast.cpp
@@ -18,11 +18,12 @@ float firstFast(float myFloat)
     // Cast address of myFloat to  intPtr and dereference *Read Float as Int*
     int temp = *(int *)&myFloat; // Bits
 
-    //-0.5 log(y)
-    temp = 0x5f3759df - (temp >> 1); //??
 
     // Start with simple notation :: find invsqrt if y = 1/sqrt y
     // log(1/sqrt y) = log (pow(y,-0.5)) = -0.5 log(y)
-    temp = 0x5f3759df - (temp >> 1);// ??
-    
+    temp = 0x5f3759df - (temp >> 1); // ??
+
+    // cast address of temp to floatPtr and dereference
+    //temp is now an approximation of the invsqrt of myFloat
+    myFloat = *(float *)&temp; // Bits
 }
