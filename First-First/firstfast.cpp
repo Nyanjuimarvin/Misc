b321f6737d2720ecf918150baa323095eff8c3f1
Nyanjuimarvin <marnyanjui@gmail.com>
2022-03-10 18:09:01 +0300

    Read values at a float memory address as an Int

--- First-First/firstfast.cpp
+++ First-First/firstfast.cpp
@@ -4,9 +4,16 @@ float firstFast(float x);
 
 int main(){
 
+    std::cout <<"Hello" << std::endl;
 }
 
+//Calculate f(x) = 1/sqrt x;
+
 float firstFast(float myFloat){
-    
+
     float halfFloat = 0.5 * myFloat;
+
+    //Cast address of myFloat to  intPtr and dereference *Read Float as Int*
+    int temp = *( int *) &myFloat;//Bits
+
 }
