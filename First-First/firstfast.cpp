797b74c9a61973b37c9dccdbb96472e102df7863
Nyanjuimarvin <marnyanjui@gmail.com>
2022-03-10 18:22:00 +0300

    Add confusing math step

--- First-First/firstfast.cpp
+++ First-First/firstfast.cpp
@@ -2,18 +2,27 @@
 
 float firstFast(float x);
 
-int main(){
+int main()
+{
 
-    std::cout <<"Hello" << std::endl;
+    std::cout << "Hello" << std::endl;
 }
 
-//Calculate f(x) = 1/sqrt x;
+// Calculate f(x) = 1/sqrt x;
 
-float firstFast(float myFloat){
+float firstFast(float myFloat)
+{
 
     float halfFloat = 0.5 * myFloat;
 
-    //Cast address of myFloat to  intPtr and dereference *Read Float as Int*
-    int temp = *( int *) &myFloat;//Bits
+    // Cast address of myFloat to  intPtr and dereference *Read Float as Int*
+    int temp = *(int *)&myFloat; // Bits
 
+    //-0.5 log(y)
+    temp = 0x5f3759df - (temp >> 1); //??
+
+    // Start with simple notation :: find invsqrt if y = 1/sqrt y
+    // log(1/sqrt y) = log (pow(y,-0.5)) = -0.5 log(y)
+    temp = 0x5f3759df - (temp >> 1);// ??
+    
 }
